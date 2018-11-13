afa0deb715d5698a909171c22b14a2d979e64e80
Aaron Myrie <aaron477myrie@gmail.com>
2018-11-13 15:56:37 +0000

    Exercise 3 and some fixes

--- search.js
+++ search.js
@@ -2,21 +2,25 @@
 
 window.onload =function(){
 
-  var button=document.getElementById('search-button');
-  button.addEventListener('click',search);
-  }
-
+  search();
+  };
 
 
 function search(){
+
+var button = document.getElementById("search-button");
+button.onclick=function(){
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://info2180-lab6-apalm03.c9users.io/request.php?q=definition");
+var word = document.getElementById("search-bar").value;
+var url = "https://info2180-lab6-apalm03.c9users.io/request.php?q=" + word;
+
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status === 200) {
-
-    var response = xhr.responseText;
-    alert(response);
+    document.getElementById("result").innerHTML=this.responseText;
   }
 }
+xhr.open("GET",url );
 xhr.send();
+
+};
 };
