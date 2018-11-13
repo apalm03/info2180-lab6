9196848fe822e0c625d5c5fc3b6c68b6e67d8385
Aaron Myrie <aaron477myrie@gmail.com>
2018-11-13 07:46:21 +0000

    minor fixes to first commit

--- search.js
+++ search.js
@@ -1,24 +1,22 @@
 
 
-window.onload = function(){
-	search();
-};
- function search(){
- 	var button = document.getElementById("search-button");
-	button.onclick = function (){
-		var request = new XMLHttpRequest();
-		var url = "https://info2180-lab6-apalm03.c9users.io/request.php?q=definition";
-		request.onreadystatechange = function (){
-			if (this.readyState == XMLHttpRequest.DONE) {
-				if (this.status == 200) {
-					var resp = this.responseText;
-					alert(resp);
-				}else{
-					console.log('There is an error ');
-				}
-			}
-		}
-		request.open('GET',url);
-		request.send();
-	}
+window.onload =function(){
+
+  var button=document.getElementById('search-button');
+  button.addEventListener('click',search);
+  }
+
+
+
+function search(){
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://info2180-lab6-apalm03.c9users.io/request.php?q=definition");
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4 && xhr.status === 200) {
+
+    var response = xhr.responseText;
+    alert(response);
+  }
 }
+xhr.send();
+};
