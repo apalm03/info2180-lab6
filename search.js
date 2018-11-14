26d8594931d2c45695f884370b9039709d0c4c3e
Aaron Myrie <aaron477myrie@gmail.com>
2018-11-14 02:28:01 +0000

    made changes to javascript code

--- search.js
+++ search.js
@@ -25,35 +25,52 @@ function search(){
     
   };
   
-  var buttonB= document.getElementById("all-def");
+ var buttonB= document.getElementById("all-def");
 	buttonB.onclick = function ()
 	{
 		var allXHR = new XMLHttpRequest();
-		var url1 ="request.php?q=&all=true";
+		var url1 ="https://info2180-lab6-apalm03.c9users.io/request.php?q=&all=true";
 		allXHR.onreadystatechange = function ()
 		{
+		var output = document.getElementById("result");
+		var order = document.createElement("ol");
+		output.innerHTML ='<h3> Result </h3>';
 			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
 			{
-			
-					var output = document.getElementById("result");
+				
 					var parser = new DOMParser();
-					 var response =  parser.parseFromString(allXHR.responseXML,'text/xml');
-					var defWord = response.getElementsByTagName('definition');
-					var order = document.createElement("ol");
-					order.setAttribute("id", "list");
+					var response =  parser.parseFromString(allXHR.responseText,'application/xml');
+					var definitions = response.getElementsByTagName('definition');
+					
+					
+					output.appendChild(order);
 					
 			
-					for (var i = 0; i < defWord.length; i++) 
+					for (var i = 0; i < definitions.length; i++) 
 					{ 
 						
-						var list = document.createAttribute("li");
-						var dict = document.createTextNode(defWord[i].children[0].childNodes[0].nodeValue);
+						var definition = document.createElement("li");
+						var heading = document.createElement('h3');
+						var p1 = document.createElement('p');
+						var p2 = document.createElement('p');
 						
-						list.appendChild(dict);
-						order.appendChild(list);
+						var word =document.createTextNode(definitions[i].getAttribute('name'));
+            	heading.appendChild(word);
+						
+						var meaning = document.createTextNode(definitions[i].childNode[0].nodeValue);
+						
+							p1.appendChild(meaning);
+					var author =document.createTextNode('-'+definitions[i].getAttribute('author'));
+            	p2.appendChild(author);
+
+                definition.appendChild(heading);
+                definition.appendChild(p1);
+                definition.appendChild(p2);
+                order.appendChild(definition);
+						
+					
 					}
 					
-					output.innerHTML=order;
 					
 				}
 				
@@ -63,9 +80,9 @@ function search(){
 		};
 		
 		allXHR.open('GET',url1,true);
+		
 		allXHR.send();
 	};
 	
 
 }
-  
