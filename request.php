8b1d97290326c422290619d4973773cdee7f7b0f
Aaron Myrie <aaron477myrie@gmail.com>
2018-11-14 00:02:41 +0000

    exercise 4 attempt and bug fixes

--- request.php
+++ request.php
@@ -1,8 +1,6 @@
 <?php
-
 // accept a term (keyword)
 // respond with a value
-
 $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
@@ -12,7 +10,56 @@ $definition = [
     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+    
 ];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition>
+        <name>definition</name>
+        <def>a statement of the exact meaning of a word, especially in a dictionary</def>
+        <author>Kim</author>
+    </definition>
+    
+    <definition>
+        <name>bar</name>
+        <def>a place that sells alcholic beverages</def>
+        <author>Mary</author>
+    </definition>
+    
+    <definition 
+        <name>ajax</name>
+        <def>technique which involves the use of javascript and xml</def>
+        <author>Latoya</author>
+    </definition>
+    
+    <definition>
+        <name>html</name>
+        <def>The standard markup language for creating web pages and web applications.</def>
+        <author>Latoya</author>
+    </definition>
+    
+    <definition>
+        <name>css</name> 
+        <def>A style sheet language used for describing the presentation of a document written in a markup language.</def>
+        <author>Tommy</author>
+    </definition>
+    
+    <definition>
+        <name>javascript</name>
+        <def>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</def>
+    </definition>
+    
+    <definition>
+        <name>php</name>
+        <def>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</def>
+        <author>Camille</author>
+    </definition>
+</entries>';
+if ($query=="&all=true"){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+} else {
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
--- search.js
+++ search.js
@@ -3,24 +3,69 @@
 window.onload =function(){
 
   search();
+
   };
 
 
 function search(){
+  var buttonA = document.getElementById("search-button");
+  buttonA.onclick=function(){
+    var xhr = new XMLHttpRequest();
+    var word = document.getElementById("search-bar").value;
+    var url = "https://info2180-lab6-apalm03.c9users.io/request.php?q=" + word;
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status === 200) {
+        document.getElementById("result").innerHTML=this.responseText;
+        
+      }
+      
+    };
+    xhr.open("GET",url );
+    xhr.send();
+    
+  };
+  
+  var buttonB= document.getElementById("all-def");
+	buttonB.onclick = function ()
+	{
+		var allXHR = new XMLHttpRequest();
+		var url1 ="request.php?q=&all=true";
+		allXHR.onreadystatechange = function ()
+		{
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
+			{
+			
+					var output = document.getElementById("result");
+					var parser = new DOMParser();
+					 var response =  parser.parseFromString(allXHR.responseXML,'text/xml');
+					var defWord = response.getElementsByTagName('definition');
+					var order = document.createElement("ol");
+					order.setAttribute("id", "list");
+					
+			
+					for (var i = 0; i < defWord.length; i++) 
+					{ 
+						
+						var list = document.createAttribute("li");
+						var dict = document.createTextNode(defWord[i].children[0].childNodes[0].nodeValue);
+						
+						list.appendChild(dict);
+						order.appendChild(list);
+					}
+					
+					output.innerHTML=order;
+					
+				}
+				
+				
+			
+			
+		};
+		
+		allRequest.open('GET',url1,true);
+		allRequest.send();
+	};
+	
 
-var button = document.getElementById("search-button");
-button.onclick=function(){
-var xhr = new XMLHttpRequest();
-var word = document.getElementById("search-bar").value;
-var url = "https://info2180-lab6-apalm03.c9users.io/request.php?q=" + word;
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4 && xhr.status === 200) {
-    document.getElementById("result").innerHTML=this.responseText;
-  }
 }
-xhr.open("GET",url );
-xhr.send();
-
-};
-};
+  
